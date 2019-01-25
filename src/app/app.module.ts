ded98799a6659d98b1bd0c944b0c143d3476d4d1
Robert Lee <bobbydwise1@hotmail.com>
2019-01-25 11:09:40 -0800

    Attempt to install Angular Firebase and dependancies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+// import { routing } from './app.routing';
+import { AppComponent } from './app.component';
+// import { WelcomeComponent } from './welcome/welcome.component';
+// import { AboutComponent } from './about/about.component';
+// import { MarketplaceComponent } from './marketplace/marketplace.component';
+// import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { PictureComponent } from './picture/picture.component';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket,
+  messagingSenderId: masterFirebaseConfig.messagingSenderId
+};
 
 @NgModule({
   declarations: [
