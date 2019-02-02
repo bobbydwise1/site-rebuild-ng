965cf32d41a5bde4fb32d1e9956d0b1aad3cf96a
bobbydwise1 <43099987+bobbydwise1@users.noreply.github.com>
2019-02-01 18:11:14 -0800

    Delete api-keys.ts

--- src/app/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export const masterFirebaseConfig = {
-  apiKey: "AIzaSyC-UekFEt2DMOh1gI6BUXzbY9KBeIb3fnc",
-  authDomain: "site-rebuild-gallery.firebaseapp.com",
-  databaseURL: "https://site-rebuild-gallery.firebaseio.com",
-  galleryId: "site-rebuild-gallery",
-  storageBucket: "",
-  messagingSenderId: "777275692735"
-};
